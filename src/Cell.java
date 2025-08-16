5b256d70d4682ae8381c0b7f1d2396098d50fc68
Yifan Cui <yifan.cui@students.mq.edu.au>
2025-08-16 23:00:55 +1000

    Add methods in Cell class

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,23 @@
-public class Cell {
-    
-}
+    import java.awt.Dimension;
+    import java.awt.Graphics;
+
+    import javax.swing.JFrame;
+    import javax.swing.JPanel;
+
+    public class Cell {
+        int cellSize;
+        int cellX;
+        int cellY;
+
+        public Cell (int s, int x, int y){
+            this.cellSize = s;
+            this.cellX = x;
+            this.cellY = y;
+        }
+
+        public void paint(Graphics g){
+            g.drawRect(cellX, cellY, cellSize, cellSize);
+            g.fillRect(cellX, cellY, cellSize, cellSize);
+        }
+
+    }
