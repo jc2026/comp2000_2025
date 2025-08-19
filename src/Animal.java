b6aeb4bac3f23e40e1708311a96aa053cfeef2de
Yifan Cui <yifan.cui@students.mq.edu.au>
2025-08-19 13:23:11 +1000

    Add setPosition method in animal class and updated cat class

--- src/Animal.java
+++ src/Animal.java
@@ -9,13 +9,27 @@ public class Animal {
 
 
     public Animal (int x, int y){
+        setPosition(x, y);
+    }
+
+    // Set the cell position
+    public void setPosition (int x, int y){
         this.row = (int) Math.ceil((double)y/size);
         this.col = (int) Math.ceil((double)x/size);
+        // if (x % size == 0){
+        //     this.col = x/35;
+        // } else {
+        //     this.col = (int) x/35 +1;
+        // } 
+        // if (y % size == 0){
+        //     this.row = y/35;
+        // } else {
+        //     this.row = (int) y/35 +1;
+        //} 
     }
-    
 
-    // locate a cell and fill colour
-   public void paint(Graphics g){
+    // Draw a cell and fill colour
+    public void paint(Graphics g){
         g.drawRect(offset + row*size, offset+col*size, size, size);
         g.setColor(Color.RED);
         g.fillRect(offset + row*size, offset+col*size, size, size);
--- src/Cat.java
+++ src/Cat.java
@@ -1,3 +1,15 @@
+import java.awt.*;
+
 public class Cat  extends Animal{
+    public Cat (int x, int y){
+        super (x, y);
+    }
+
+    @Override
+    public void paint(Graphics g){
+        g.drawRect(offset + row*size, offset+col*size, size, size);
+        g.setColor(Color.BLUE);
+        g.fillRect(offset + row*size, offset+col*size, size, size);
+    } 
     
 }
